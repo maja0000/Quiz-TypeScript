df537f8737411674279577bb737b4856393bb33e
Maja Gach <majaanna94@gmail.com>
2020-08-03 19:58:34 +0200

    working version

---
+++ src/App.styles.ts
@@ -0,0 +1,61 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import BGImage from './images/osiedle-plac-grunwaldzki.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+
+  * {
+    font-family: 'Catamaran', sans-serif;
+    box-sizing: border-box;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  h1 {
+    color: salmon;
+    font-weight: 400;
+    font-size: 50px;
+  }
+
+  .start,
+  .next {
+    cursor: pointer;
+    color: white;
+    background: grey;
+    border: 2px solid salmon;
+    box-shadow: yellow;
+    border-radius: 10px;
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
+
+  .start {
+    max-width: 200px;
+  }
+`;
